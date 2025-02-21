fac7d000daa53c0cbc38f4afb0a8c1fb551bec6f
Josue Enmanuel Bernabe Marte <josuebernabe929@gmail.com>
2025-02-20 23:40:44 -0400

    Commission Calculator for Employees
    This basic commission calculator is one of my first projects in Python, as I am just starting to learn the language. While simple, this program marks the beginning of my journey in Python programming, helping me understand fundamental concepts such as user input, mathematical operations, and formatted output.

---
+++ Calculador_de_comisiones.py
@@ -0,0 +1,18 @@
+nombre = input("Por favor, Digite su nombre:  ")
+ventas = int(input("Digite sus ventas totales de este mes:"))
+
+comisiones = round(ventas * 13 / 100,2)
+
+print(f"Hola {nombre} tus comisiones de este mes ${comisiones}")
+
+
+
+
+
+
+
+
+
+
+
+

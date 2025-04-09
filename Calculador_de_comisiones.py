621e42573c4c9fa923e4d5127ea400823fc30e02
Josue Enmanuel Bernabe Marte <josuebernabe929@gmail.com>
2025-04-09 14:29:28 -0400

    Update Calculador_de_comisiones.py

--- Calculador_de_comisiones.py
+++ Calculador_de_comisiones.py
@@ -1,18 +1,40 @@
-nombre = input("Por favor, Digite su nombre:  ")
-ventas = int(input("Digite sus ventas totales de este mes:"))
+class Vendedor:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
 
-comisiones = round(ventas * 13 / 100,2)
-
-print(f"Hola {nombre} tus comisiones de este mes ${comisiones}")
-
-
-
+    def nombre_completo(self):
+        return f"{self.nombre} {self.apellido}"
 
 
+class Calculador(Vendedor):
+    def __init__(self, nombre, apellido, porcentaje_comision=13):
+        super().__init__(nombre, apellido)
+        self.porcentaje_comision = porcentaje_comision
+        self.ventas = 0
+        self.comision = 0
+
+    def registrar_ventas(self, monto):
+        if monto >= 0:
+            self.ventas = monto
+            self.calcular_comision()
+            return True
+        return False
+
+    def calcular_comision(self):
+        self.comision = round(self.ventas * self.porcentaje_comision / 100, 2)
+
+    def mostrar_comision(self):
+        return f"Hola {self.nombre_completo()}, tus comisiones de este mes son: ${self.comision}"
 
 
 
+nombre = input("Bienvenido\nPor favor, digite su nombre: ")
+apellido = input("Digite su apellido: ")
+ventas = float(input("Digite sus ventas totales de este mes en $: "))
 
-
-
-
+vendedor = Calculador(nombre, apellido)
+if vendedor.registrar_ventas(ventas):
+    print(vendedor.mostrar_comision())
+else:
+    print("Las ventas no pueden ser negativas.")
